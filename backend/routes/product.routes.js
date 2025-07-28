354be941eafde61ec4d8e5b70fdd4dd03e073239
szkajvolker <ist9595@windowslive.com>
2025-07-28 16:51:07 +0200

    fixed wrong file changes

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -2,3 +2,11 @@ import express from "express";
 import {} from "..controllers/product.controllers.js";
 
 const router = express.Router();
+
+router.post("/", addNewProduct);
+router.get("/", getAllProducts);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+router.get("/:id", getProduct);
+
+export default router;
